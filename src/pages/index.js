dc29fd4c3778f2bd6e10d4f1de0f1e8a22a20952
Marcos Orive <marcos@orive.me>
2019-08-14 10:28:19 +0200

    Adjust header height. Other small visual changes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,10 @@ export default class index extends React.Component{
         return(
           <Layout title="Home" headerText="Hi, I'm Marcos">
               <div className="index-container">
-                <div className="index-cheer">I'm a software engineer based in Spain</div>
-                <div className="index-description">Here you can check out some of my work, my blog posts and social links. Feel free to contact me if you like.</div>
+                  <div className="index-text-container">
+                    <div className="index-cheer">I'm a software engineer based in Spain</div>
+                    <div className="index-description">Here you can check out some of my work, my blog posts and social links. Feel free to contact me if you like.</div>
+                  </div>
                 <div className="index-social-links">
                   <Button to="https://github.com/marcosorive">Github</Button>
                   <Button to="https://twitter.com/marcosorive">twitter</Button>
