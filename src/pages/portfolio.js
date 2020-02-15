291183b1fc0fb7cb899d31cc62691b6b7fead8b7
Marcos Orive <marcos@orive.me>
2020-02-15 21:10:57 +0100

    Refactor the way cards are rendered in portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -5,142 +5,98 @@ import {Button} from '../components/Button/Button';
 import './css/portfolio.css'
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import projectsContent from '../../content/portfolio/projects.json';
+import experiencesContent from '../../content/portfolio/experience.json';
 
-export default class portfolio extends React.Component{
+export default class Portfolio extends React.Component{	
+
+	constructor(props){
+		super(props)
+		this.state = {
+			cards: undefined,
+			experiences: undefined
+		}
+		this.setProjectCardsContent = this.setProjectCardsContent.bind(this);
+		this.setExperiencieCardsContent = this.setExperiencieCardsContent.bind(this);
+	}
+
+	componentDidMount(){
+		this.setProjectCardsContent();
+		this.setExperiencieCardsContent();
+	}
+
     render(){
         return(
 			<Layout title="Portfolio">
 				<div className="card-layout">
-				<Card>
-						<CardImageHeader><Img fluid={this.props.data.ubureactvr.childImageSharp.fluid} alt="Nintendo switch"/></CardImageHeader>
-						<CardBody>
-							<h2>UBU ReactVR</h2>
-							<div className="card-body-stack">Node.js with typescript, React, React Native, React360.</div>
-							<hr className="card-body-hr"/>
-							<p className="card-body-description">My final degree project. Web, smartphone and VR/300 apps with a Node.js server. Manage and inspect 3D models in VR. Made in colaboration with <a href="https://hpscds.com/" rel="noopener noreferrer" target="_blank">HP SCDS.</a></p>						
-						</CardBody>
-						<CardFooter><Button to="https://hpscds.com/observatorio_proyectos/reactvr/">More info</Button></CardFooter>				
-					</Card>
-					<Card>
-						<CardImageHeader><Img fluid={this.props.data.nSwitch.childImageSharp.fluid} alt="Nintendo switch calendar"/></CardImageHeader>
-						<CardBody>
-							<h2>Switch Calendar</h2>
-							<div className="card-body-stack">Python, Django, MySQL, MDBootstrap.</div>
-							<hr className="card-body-hr"/>
-							<p className="card-body-description">Web app to keep track the Nintendo Switch games releases. Users can register/log in and create their own calendar.</p>						
-						</CardBody>
-						<CardFooter>
-							<Button to="https://github.com/marcosorive/switchcalendar">Code</Button>
-							<Button to="https://switchcalendar.pythonanywhere.com">Demo</Button>
-						</CardFooter>
-					</Card>
-					<Card>
-						<CardImageHeader><Img fluid={this.props.data.anotherTaco.childImageSharp.fluid} alt="Another taco, please!"/></CardImageHeader>
-						<CardBody>
-							<h2>Another taco, please!</h2>
-							<div className="card-body-stack">ReactJS, MDBootstrap.</div>
-							<hr className="card-body-hr"/>
-							<p className="card-body-description">Frontend project for tacofancy-api, displays random taco recipes.</p>
-						</CardBody>
-						<CardFooter>
-							<Button to="https://github.com/marcosorive/anothertacoplease">Code</Button>
-							<Button to="https://marcosorive.github.io/anothertacoplease/">Demo</Button>
-						</CardFooter>
-					</Card>
-					<Card >
-						<CardImageHeader><Img fluid={this.props.data.apodReact.childImageSharp.fluid} alt="Apod React"/> </CardImageHeader>
-						<CardBody>
-							<h2>Astronomic picture of the day</h2>
-							<div className="card-body-stack">ReactJS, Bootstrap</div>
-							<hr className="card-body-hr"/>
-							<p className="card-body-description">React project that displays NASA's Astronomic picture of the day</p>
-						</CardBody>
-						<CardFooter>
-							<Button to="https://github.com/marcosorive/apod-react">Code</Button>
-							<Button to="https://marcosorive.github.io/apod-react/">Demo</Button>
-						</CardFooter>
-					</Card> 
-					<Card>
-						<CardImageHeader><Img fluid={this.props.data.torky.childImageSharp.fluid} alt="Torky"/></CardImageHeader>
-						<CardBody>
-							<h2>Torky</h2>
-							<div className="card-body-stack">Python, Flask, ReactJS, Bootstrap.</div>
-							<hr className="card-body-hr"/>
-							<p className="card-body-description">Videogame price comparator for Spanish stores. Uses webscrapping and Amazon API.</p>
-						</CardBody>
-						<CardFooter>
-							<Button to="https://github.com/marcosorive/torky-api">Code</Button>
-							<Button to="https://marcosorive.github.io/torky/">Demo</Button>
-						</CardFooter>
-					</Card>
-					<Card>
-						<CardImageHeader><Img fluid={this.props.data.torkynative.childImageSharp.fluid} alt="Torky native screenshot"/></CardImageHeader>
-						<CardBody>
-							<h2>Torky Native</h2>
-							<div className="card-body-stack">React native</div>
-							<hr className="card-body-hr"/>
-							<p className="card-body-description">Smartphone for Torky, made with React Native. It will be avaiible in the Google Play store as soon as Google gets it approved.</p>
-						</CardBody>
-						<CardFooter>
-							<Button to="https://github.com/marcosorive/torky-native">Code</Button>
-							<Button to="https://play.google.com/store/apps/details?id=me.orive.marcos.torkynative">Google Play</Button>
-						</CardFooter>
-					</Card>
-					<Card>
-						<CardBody>
-							<h2>Blog and portfolio</h2>
-							<div className="card-body-stack">Gatsby, Vanilla CSS, Netlify.</div>
-							<hr className="card-body-hr"/>
-							<p className="card-body-description">This website that you are seeing right now. Serves as my portfolio and blog. </p>
-						</CardBody>
-						<CardFooter>
-							<Button to="https://github.com/marcosorive/blog-and-portfolio">Code</Button>
-						</CardFooter>
-					</Card> 
+					{this.state.cards}
 				</div>
 				<hr className="portfolio-hr"/>
 				<h1 className="text-center">Professional experience</h1>
 				<div className="card-layout">
-					<Card>
-						<CardImageHeader><Img fluid={this.props.data.raquel.childImageSharp.fluid} alt="Raquel Vazquez screenshot"/> </CardImageHeader>
-						<CardBody>
-							<h2>Inmobiliaria Raquel Vázquez</h2>
-							<div className="card-body-stack">Python, Django, MySQL, Bootstrap.</div>
-							<hr className="card-body-hr"/>
-							<p className="card-body-description">Web app for internal data managment. Working with a legacy DB.</p>
-						</CardBody>
-						<CardFooter>
-							<Button to="https://www.inmobiliariaraquelvazquez.com">Visit</Button>
-						</CardFooter>
-					</Card>
-					<Card>
-						<CardImageHeader><Img fluid={this.props.data.csa.childImageSharp.fluid} alt="Csa logo"/> </CardImageHeader>
-						<CardBody>
-							<h2>CSA.</h2>
-							<div className="card-body-stack">Software developer</div>
-							<hr className="card-body-hr"/>
-							<p className="card-body-description"> Worked in a legacy ticketing system.</p>
-						</CardBody>
-						<CardFooter>
-							<Button to="https://www.csa.es/">Visit</Button>
-						</CardFooter>
-					</Card>
-					<Card>
-						<CardImageHeader><Img fluid={this.props.data.hpscds.childImageSharp.fluid} alt="HP Logo"/> </CardImageHeader>
-						<CardBody>
-							<h2>HP SCDS</h2>
-							<div className="card-body-stack">Software engineer.</div>
-							<hr className="card-body-hr"/>
-							<p className="card-body-description">Developing software for large format printers.</p>
-						</CardBody>
-						<CardFooter>
-							<Button to="https://www.csa.es/">Visit</Button>
-						</CardFooter>
-					</Card>
-				</div>
+					{this.state.experiences}
+				</div> 
 			</Layout>
         )
-    }
+	}
+
+	setProjectCardsContent(){
+		const cards = projectsContent.map(cardContent => {
+			return(
+				<Card key={cardContent.image}>
+					<CardImageHeader>
+						{ cardContent.image ?
+							<Img fluid={this.props.data[cardContent.image].childImageSharp.fluid} alt={cardContent.altImage}/> :
+							<></>
+						}						
+					</CardImageHeader>
+					<CardBody>
+						<h2>{cardContent.title}</h2>
+						<div className="card-body-stack">{cardContent.stack}</div>
+						<hr className="card-body-hr"/>
+						<p className="card-body-description" dangerouslySetInnerHTML={ { __html: cardContent.description } }></p>						
+					</CardBody>
+					<CardFooter>
+						{cardContent.buttons.map(button => {
+							return <Button key={button.text} to={button.url}>{button.text}</Button>
+						})}
+					</CardFooter>				
+				</Card>
+			)
+		})
+		this.setState({
+			cards: cards
+		})
+	}
+
+	setExperiencieCardsContent(){
+		const experiences = experiencesContent.map(cardContent => {
+			return(
+				<Card key={cardContent.image}>
+					<CardImageHeader>
+						{ cardContent.image ?
+							<Img fluid={this.props.data[cardContent.image].childImageSharp.fluid} alt={cardContent.altImage}/> :
+							<></>
+						}						
+					</CardImageHeader>
+					<CardBody>
+						<h2>{cardContent.title}</h2>
+						<div className="card-body-stack">{cardContent.stack}</div>
+						<hr className="card-body-hr"/>
+						<p className="card-body-description" dangerouslySetInnerHTML={ { __html: cardContent.description } }></p>						
+					</CardBody>
+					<CardFooter>
+						{cardContent.buttons.map(button => {
+							return <Button key={button.text} to={button.url}>{button.text}</Button>
+						})}
+					</CardFooter>				
+				</Card>
+			)
+		})
+		this.setState({
+			experiences: experiences
+		})
+	}
 }
 
 export const pageQuery = graphql`
