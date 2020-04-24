780e654bf1db70671884c0ce1c4376a550039f4b
Marcos Orive Izarra <marcos@orive.me>
2020-04-24 10:21:35 +0200

    Form working

---
+++ src/pages/contact.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Layout from '../components/Layout/Layout';
+import './css/contact.css'
+
+export default class contact extends React.Component{
+
+    render(){
+        return(
+            <Layout title="Contact" headerText="Contact me!">
+                <div class="form-wrapper">
+                    <form class="form" name="contact" method="POST" data-netlify="true">    
+                            <div class="name-email-wrapper">                
+                                <div>
+                                    <label for="name">Your Name: </label>
+                                    <input type="text" name="name" id="name" placeholder="John Doe" required/>                  
+                                </div>
+                                <div>
+                                    <label>Your Email: </label>
+                                    <input type="email" name="email" placeholder="john@email.com" required/>           
+                                </div>
+                            </div>
+                            <div className="comment-wrapper">
+                                <label>Message: </label>
+                                <textarea name="message" rows="7" cols="30" required></textarea>            
+                            </div>
+                            <button type="submit">Send</button>            
+                    </form>
+                </div>
+            </Layout>
+        )
+    }
+}
