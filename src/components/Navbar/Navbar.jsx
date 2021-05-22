631c1d2a33b1312b463a84ca810f120a167b7714
Marcos Orive <marcos@orive.me>
2021-05-22 17:40:52 +0200

    new fix for window object in build time

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,44 +1,42 @@
 import React from 'react';
-import {Link} from 'gatsby';
+import { Link } from 'gatsby';
 
 import './Navbar.scss'
 
 const isBrowser = typeof window !== "undefined"
 
-export function Navbar (props){
-    if(!isBrowser){
-        return <div>You need a browser</div>
-    }
-    if(window.location.pathname === '/' || window.location.pathname === '' ){
-        return(
+export function Navbar(props) {
+
+    if (!isBrowser || window.location.pathname === '/' || window.location.pathname === '') {
+        return (
             <nav className="nav__wrapper">
-                {props.links.map((link) =>{
-                    if(link.link.startsWith("#")){
+                {props.links.map((link) => {
+                    if (link.link.startsWith("#")) {
                         return <a className="nav__link" key={link.text} href={`/${link.link}`} >{link.text}</a>
-                    }else{
-                        return(<Link className="nav__link" key={link.text} to={link.link} >{link.text}</Link>);
+                    } else {
+                        return (<Link className="nav__link" key={link.text} to={link.link} >{link.text}</Link>);
                     }
                 })}
             </nav>
-            )   
-    }else {
+        )
+    } else {
         return (
             <nav className="nav__wrapper">
                 <Link className="nav__link" to='/'>Home</Link>
                 <Link className="nav__link" to='/blog'>Blog</Link>
-        </nav>
+            </nav>
         )
     }
 }
 
 Navbar.defaultProps = {
-    backgroundColor : 'black',
+    backgroundColor: 'black',
     fontColor: 'white',
     brand: '',
-    links:[
-        {text: "Home", link:"/"},
-        {text: "Portfolio", link:"/portfolio"},
-        {text: "Blog", link:"/blog"},
-        {text: "Contact", link:"/contact"}
+    links: [
+        { text: "Home", link: "/" },
+        { text: "Portfolio", link: "/portfolio" },
+        { text: "Blog", link: "/blog" },
+        { text: "Contact", link: "/contact" }
     ]
 }
